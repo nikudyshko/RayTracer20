7eac898654c8cd9cad588aa5503533a66036e331
nikudyshko <lishuk@i.ua>
2020-06-03 23:35:29 +0300

    Testing Vec functionality

--- Application.cpp
+++ Application.cpp
@@ -4,13 +4,9 @@ import std.core;
 
 int main() 
 { 
-	Vec<int> v0; 
-	Vec<int> v1(1); 
-	Vec<int> v2(1, 2, 3); 
-	Vec<int> v3(1, 2, 3, 4); 
-	Vec<int> v4(v1); 
-	std::cout << v0 << '\n'; 
-	/*std::cout << v0 << '\n' << v1 << '\n' << v2 << '\n' 
-			  << v3 << '\n' << v4 << '\n'; */
+	Vec<int> v1(1, 2, 3); 
+	Vec<float> v2(4.0f, 5.0f, 6.0f); 
+	auto v3 = v1 ^ v2; 
+	std::cout << v1[0] << ' ' << v1[1] << ' ' << v1[2] << ' ' << v1[3] << '\n';  
 	return 0; 
 } 
