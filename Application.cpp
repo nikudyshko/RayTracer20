6d9500858689193e089821a8f93e74c8b98f4c00
nikudyshko <lishuk@i.ua>
2020-06-04 23:35:35 +0300

    Cleaned up after experiments

--- Application.cpp
+++ Application.cpp
@@ -3,10 +3,6 @@ import saveppm;
 import std.core; 
 
 int main() 
-{ 
-	Vec<int> v1(1, 2, 3); 
-	Vec<float> v2(4.0f, 5.0f, 6.0f); 
-	auto v3 = v1 ^ v2; 
-	std::cout << v1[0] << ' ' << v1[1] << ' ' << v1[2] << ' ' << v1[3] << '\n';  
+{  
 	return 0; 
 } 
