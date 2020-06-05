a6b8534bacc29d1e6477b12ca7a7b02897b7aa48
nikudyshko <lishuk@i.ua>
2020-06-05 23:56:57 +0300

    Now script builds and links the whole project

--- build_script.bat
+++ build_script.bat
@@ -1,2 +1,4 @@
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD vec.ixx 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD saveppm.ixx 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD application.ixx 
+cl.exe application.obj vec.obj saveppm.obj 
