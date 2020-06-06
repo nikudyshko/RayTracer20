04dbf21b695e034b3e711572d1e4a1109d963811
nikudyshko <lishuk@i.ua>
2020-06-07 01:54:27 +0300

    Added polygon module to build process

--- build_script.bat
+++ build_script.bat
@@ -1,4 +1,5 @@
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD vec.ixx 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD polygon.ixx 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD saveppm.ixx 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD application.ixx 
-cl.exe application.obj vec.obj saveppm.obj 
+cl.exe application.obj vec.obj polygon.obj saveppm.obj 
