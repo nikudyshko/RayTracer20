fdf11387c56eb43735fccec54232a073833b119f
nikudyshko <lishuk@i.ua>
2020-06-15 01:40:09 +0300

    Added all files to build process

--- build_script.bat
+++ build_script.bat
@@ -1,5 +1,10 @@
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD render_constants.ixx 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD vec.ixx 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD ray.ixx 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD material.ixx 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD polygon.ixx 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD shell.ixx  
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD camera.ixx 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD saveppm.ixx 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD application.ixx 
-cl.exe application.obj vec.obj polygon.obj saveppm.obj 
+cl.exe application.obj vec.obj ray.obj material.obj polygon.obj shell.obj camera.obj saveppm.obj 
