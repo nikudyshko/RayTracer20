af6ae88b14882338bb6f7259be00fa4d452126f5
nikudyshko <lishuk@i.ua>
2020-07-23 22:37:25 +0300

    Changed paths to new folder structure

--- build_script.bat
+++ build_script.bat
@@ -1,16 +1,17 @@
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD render_constants.ixx 
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD custom_concepts.ixx 
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD vec.ixx 
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD matrix.ixx 
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD light.ixx 
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD ray.ixx 
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD material.ixx 
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD polygon.ixx 
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD surface.ixx 
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD shell.ixx  
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD camera.ixx 
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD saveppm.ixx 
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD renderer.ixx 
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD test.ixx 
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD application.ixx 
-cl.exe application.obj render_constants.obj custom_concepts.obj vec.obj matrix.obj light.obj ray.obj material.obj polygon.obj surface.obj shell.obj camera.obj saveppm.obj renderer.obj test.obj 
+mkdir obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/render_constants.ixx /Foobj/render_constants.obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/custom_concepts.ixx /Foobj/custom_concepts.obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/vec.ixx /Foobj/vec.obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/matrix.ixx /Foobj/matrix.obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/light.ixx /Foobj/light.obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/ray.ixx /Foobj/ray.obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/material.ixx /Foobj/material.obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/polygon.ixx /Foobj/polygon.obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/surface.ixx /Foobj/surface.obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/shell.ixx /Foobj/shell.obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/camera.ixx /Foobj/camera.obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/saveppm.ixx /Foobj/saveppm.obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/renderer.ixx /Foobj/renderer.obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/test.ixx /Foobj/test.obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD application.ixx /Foobj/application.obj 
+cl.exe obj/application.obj obj/render_constants.obj obj/custom_concepts.obj obj/vec.obj obj/matrix.obj obj/light.obj obj/ray.obj obj/material.obj obj/polygon.obj obj/surface.obj obj/shell.obj obj/camera.obj obj/saveppm.obj obj/renderer.obj obj/test.obj 
