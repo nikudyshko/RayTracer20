e46645b1f6bdd95144bcde6dc7c778aadee3f53c
nikudyshko <lishuk@i.ua>
2020-07-27 00:38:06 +0300

    Changed compilation order

--- build_script.bat
+++ build_script.bat
@@ -4,8 +4,8 @@ cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/custom_concepts.ixx 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/vec.ixx /Foobj/vec.obj 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/matrix.ixx /Foobj/matrix.obj 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/light.ixx /Foobj/light.obj 
-cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/ray.ixx /Foobj/ray.obj 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/material.ixx /Foobj/material.obj 
+cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/ray.ixx /Foobj/ray.obj 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/polygon.ixx /Foobj/polygon.obj 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/surface.ixx /Foobj/surface.obj 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/shell.ixx /Foobj/shell.obj 
@@ -14,4 +14,4 @@ cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/saveppm.ixx /Foobj/s
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/renderer.ixx /Foobj/renderer.obj 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD src/test.ixx /Foobj/test.obj 
 cl.exe /std:c++latest /experimental:module /c /EHsc /MD application.ixx /Foobj/application.obj 
-cl.exe obj/application.obj obj/render_constants.obj obj/custom_concepts.obj obj/vec.obj obj/matrix.obj obj/light.obj obj/ray.obj obj/material.obj obj/polygon.obj obj/surface.obj obj/shell.obj obj/camera.obj obj/saveppm.obj obj/renderer.obj obj/test.obj 
+cl.exe obj/application.obj obj/render_constants.obj obj/custom_concepts.obj obj/vec.obj obj/matrix.obj obj/light.obj obj/material.obj obj/ray.obj obj/polygon.obj obj/surface.obj obj/shell.obj obj/camera.obj obj/saveppm.obj obj/renderer.obj obj/test.obj 
